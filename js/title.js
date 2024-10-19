1e05fd231a2b70faeba50c411d7e4faca3ab9247
IoTEgr <3224234496@qq.com>
2024-10-19 14:37:17 +0800

    Site updated: 2024-10-19 14:37:16

--- js/title.js
+++ js/title.js
@@ -4,11 +4,11 @@ var titleTime;
 document.addEventListener("visibilitychange", function () {
   if (document.hidden) {
     //离开当前页面时标签显示内容
-    document.title = "👀跑哪里去了~";
+    document.title = "你干嘛~ 诶呦";
     clearTimeout(titleTime);
   } else {
     //返回当前页面时标签显示内容
-    document.title = "🐖抓到你啦～";
+    document.title = "哇~ 真的是你呀~";
     //两秒后变回正常标题
     titleTime = setTimeout(function () {
       document.title = OriginTitile;
